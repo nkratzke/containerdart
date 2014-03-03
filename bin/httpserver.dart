13e849b5ba223773355f8e53c9fdd1701c309ce2
Nane Kratzke <nane.kratzke@googlemail.com>
2014-03-03 12:16:49 +0100

    Listen to 0.0.0.0

--- bin/httpserver.dart
+++ bin/httpserver.dart
@@ -1,7 +1,7 @@
 import 'package:start/start.dart';
 
 main() {
-  start(port: 8080).then((Server app) {
+  start(host: '0.0.0.0', port: 8080).then((Server app) {
     app.static('../build/web');
   });
 }
